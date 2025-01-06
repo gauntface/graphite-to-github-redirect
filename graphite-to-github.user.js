caf575d65c68df59af9d6932f16bd6bc86217f12
Matt Gaunt-Seo <matt@gaunt.dev>
2025-01-06 09:53:34 -0800

    Add user script

---
+++ graphite-to-github.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Redirect from Graphite to GitHub
+// @namespace    https://gaunt.dev/
+// @version      2025-01-06
+// @description  Redirect from Graphite to GitHub
+// @author       gauntface
+// @match        https://app.graphite.dev/github/pr/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=graphite.dev
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // URL Format: https://app.graphite.dev/github/pr/<org>/<repo>/<PR Number>/<PR Title>
+  const href = window.location.href.trim();
+  const regex = /https:\/\/app.graphite.dev\/github\/pr\/([^/]+)\/([^/]+)\/(\d+)\/.*/;
+  const match = href.match(regex);
+  if (!match) {
+      return;
+  }
+  const org = match[1];
+  const repo = match[2];
+  const prNumber = match[3];
+  const gitHubURL = `https://github.com/${org}/${repo}/pull/${prNumber}`;
+  console.log(`Redirecting from Graphite to GitHub.\n\n    Graphite: ${href}\n    GitHub: ${gitHubURL}`);
+  window.location.href = gitHubURL;
+})();
