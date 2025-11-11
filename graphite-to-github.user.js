098c5761e8aa4b6acaf219619fefc3991a33914e
Matt Gaunt-Seo <matt@gaunt.dev>
2025-11-11 14:56:24 -0800

    Fix graphite to github for com

--- graphite-to-github.user.js
+++ graphite-to-github.user.js
@@ -5,6 +5,7 @@
 // @description  Redirect from Graphite to GitHub
 // @author       gauntface
 // @match        https://app.graphite.dev/github/pr/*
+// @match        https://app.graphite.com/github/pr/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=graphite.dev
 // @grant        none
 // ==/UserScript==
